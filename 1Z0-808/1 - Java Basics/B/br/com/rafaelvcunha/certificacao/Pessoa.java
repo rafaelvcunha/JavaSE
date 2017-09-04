f72eaa935c73972171b53ce8f9bc7d440588188a
rafaelvcunha <rafaelvcunha@gmail.com>
2017-09-04 08:48:40 -0300

    Estudos Defina a estrutura de uma classe Java

---
+++ 1Z0-808/1 - Java Basics/B/br/com/rafaelvcunha/certificacao/Pessoa.java
@@ -0,0 +1,17 @@
+// Declaração de pacote
+package br.com.rafaelvcunha.certificacao;
+/**
+Error: Could not find or load main class Pessoa
+ */
+
+// imports
+import java.util.Date;
+
+// Declaração da classe
+class Pessoa {
+    // conteúdo da classe
+
+    public static void main(String[]args){
+
+    }
+}
