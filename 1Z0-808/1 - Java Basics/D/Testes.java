2ec270d5afb7d486aac0c06536153c5795bc03ce
rafaelvcunha <rafaelvcunha@gmail.com>
2017-09-08 09:56:00 -0300

    Finalizado capitulo 1

---
+++ 1Z0-808/1 - Java Basics/D/Testes.java
@@ -0,0 +1,11 @@
+// Importando todos os membros public static de Utils
+import static model.Utils.*;
+
+public class Testes {
+
+    public static void main(String[] args) {
+        int x = VALOR;
+        metodo1();
+        metodo1(x);
+    }
+}
